26144c1a71333368d59c298eddacc62e3ba09b4d
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-10-24 12:06:57 -0700

    Add empty_cell method

--- source/sudoku.rb
+++ source/sudoku.rb
@@ -1,5 +1,23 @@
 class Sudoku
   def initialize(board_string)
+    #@board_string = board_string.split("")
+    @board_string = board_string
+  end
+
+  def board_convert
+    @board_array = @board_string.split("").map(&:to_i)
+  end
+
+  def empty_cell
+    @empty_cell_indices = []
+   # @board_string.map! { |item| item.to_i }
+   # @board_string.each_with_index { |item, index| empty_cell_indices << index if item.zero? }
+   @board_array.each_with_index { |item, index| @empty_cell_indices << index if item.zero? }
+    p @empty_cell_indices
+  end
+
+  def empty_cell_indices
+
   end
 
   def solve
@@ -12,3 +30,9 @@ class Sudoku
   def to_s
   end
 end
+
+
+#####DRIVER CODE#########
+game = Sudoku.new("1-58-2----9--764-52--4--819-19--73-6762-83-9-----61-5---76---3-43--2-5-16--3-89--")
+game.board_convert
+game.empty_cell
